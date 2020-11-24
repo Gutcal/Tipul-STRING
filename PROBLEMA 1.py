337ed8b424dbbe07d2704849b74f4da565fb14d1
Gutcal <70690295+Gutcal@users.noreply.github.com>
2020-11-24 12:08:57 +0300

    Add files via upload

---
+++ PROBLEMA 1.py
@@ -0,0 +1,4 @@
+s=n
+print(chr(65<=91))
+print(chr(48<=57))
+print(chr(33<=47 and 58<=64 and 91<=96 and 123<=127))
---
+++ problema 2.py
@@ -0,0 +1,5 @@
+c='Ana'
+a='are'
+s=25
+b='ani'
+print(c+" "+a+" "+str(s)+" "+b+chr(46))
